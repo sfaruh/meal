83a69cee503fd82116bed0a7e174fe858f066128
sfaruh <s.faruh@icloud.com>
2019-02-26 10:40:02 +0600

    Fix first page and make timer interface and autolayout

---
+++ StopWatch/StopWatch/TimerViewController.swift
@@ -0,0 +1,9 @@
+//
+//  TimerViewController.swift
+//  StopWatch
+//
+//  Created by Farukh Sharipov on 26/02/2019.
+//  Copyright © 2019 Farukh Sharipov. All rights reserved.
+//
+
+import Foundation
